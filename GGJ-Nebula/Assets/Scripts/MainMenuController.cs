d640c2a3daeda7804d93c44c73a66fc3255c1b83
Kenesthor <48281757+Kenesthor@users.noreply.github.com>
2025-01-25 14:45:45 +0100

    Menu Principal
    Añadida escena del menu principal con funcionalidad de botones y menu de pausa
    Falta por añadir efecto fade-in/fade-out al cargar niveles y continuar con el diseño del menu de pausa.

--- GGJ-Nebula/Assets/Scripts/MainMenuController.cs
+++ GGJ-Nebula/Assets/Scripts/MainMenuController.cs
@@ -1,16 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+    public GameObject creditsUI;
+
     void Start()
     {
-        
+        gameIsPaused = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!gameIsPaused) {
+                Pause();
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Resume();
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void SeeCredits()
+    {
+        creditsUI.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsUI.SetActive(false);
+    }
+
+    public void StartGame()
+    {
         
+        SceneManager.LoadScene(1);
+    }
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
